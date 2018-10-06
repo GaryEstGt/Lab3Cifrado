c0f359f17b94f3c9e55acfb6e7424870cff2a411
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2018-10-06 13:13:48 -0600

    menu y activities creadas

--- app/src/main/java/com/example/garya/lab3cifrado/MainActivity.java
+++ app/src/main/java/com/example/garya/lab3cifrado/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,6 +37,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_CZigzag:
+                Toast.makeText(this.getApplicationContext(), "Ya esta en Cifrar zig zag", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_DZigzag:
+                Intent intentDZig = new Intent(MainActivity.this, DZigzag.class);
+                startActivity(intentDZig);
+                return true;
+            case R.id.menu_CTransposicion:
+                Intent intentCT = new Intent(MainActivity.this, CTransposicion.class);
+                startActivity(intentCT);
+                return true;
+            case R.id.menu_DTransposicion:
+                Intent intentDT = new Intent(MainActivity.this, DTransposicion.class);
+                startActivity(intentDT);
+                return true;
+            case R.id.menu_CCesar:
+                Intent intentCC = new Intent(MainActivity.this, CCesar.class);
+                startActivity(intentCC);
+                return true;
+            case R.id.DCesar:
+                Intent intentDC= new Intent(MainActivity.this, DCesar.class);
+                startActivity(intentDC);
+                return true;
+            case R.id.Salir:
+                finish();
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @OnClick({R.id.btnAbrirArchivo, R.id.btnCifrar})
